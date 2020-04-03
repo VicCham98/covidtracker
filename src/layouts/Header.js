f656d5e5bee1c249f3d15dfe3aa7f83f29c4e601
VicCham98 <victor_cha_q@hotmail.com>
2020-04-03 16:14:10 -0500

    fixed top navBar

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -15,19 +15,19 @@ const Header = () => {
     const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <Navbar color="light" light expand="md">
+        <Navbar color="light" light expand="md" fixed="top">
             <NavbarBrand href="/">COVID-19 Tracker</NavbarBrand>
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                     <NavItem>
-                        <NavLink href="/">Datos</NavLink>
+                        <NavLink disabled href="/">Datos</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="/">Acerca del COVID-19</NavLink>
+                        <NavLink disabled href="/">Acerca del COVID-19</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink href="/">Sintomas</NavLink>
+                        <NavLink disabled href="/">Sintomas</NavLink>
                     </NavItem>
                 </Nav>
             </Collapse>
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,7 +3,7 @@ import Container from "reactstrap/es/Container";
 
 const Layout = (props) => {
     return (
-        <Container fluid={true}>
+        <Container fluid={true} className="mt-5 pt-1">
             {props.children}
         </Container>
     );
